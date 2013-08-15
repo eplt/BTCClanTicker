5ff7c1284e448a5e88f5e9b14d2e44d2099f28db
ibittler <ibittler@outlook.com>
2013-08-15 23:36:53 +0200

    Initial code commit

---
+++ CoinNotify/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoinNotify
+//
+//  Created by Francesc Bruguera Moriscot on 04/08/13.
+//  Copyright (c) 2013 ibittler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
